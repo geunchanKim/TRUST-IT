d45cdef36b0cf129ce1c41cec76396a73830d84c
dahyun0917 <dahyun0917@naver.com>
2022-05-26 21:21:27 +0900

    오류 수정

--- Trust_it/urls.py
+++ Trust_it/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',  admin.site.urls),
     path('communication/',include('communication.urls')),
-    path('analysisproducts/',include('analysisproducts.urls')),
-    path('notification/',include('notification.urls')),
+    #path('analysisproducts/',include('analysisproducts.urls')),
+    #path('notification/',include('notification.urls')),
 ]
--- communication/views.py
+++ communication/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+#from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    
+    return render(
+        request,
+        'communication/post_list.html',
+        # {
+        #     'posts' : posts,
+        # }
+    )
