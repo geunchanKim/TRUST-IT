086f37355bd2880c43c9e1363d91b2fa50fb2315
mouse4432 <88102469+mouse4432@users.noreply.github.com>
2022-05-27 07:31:33 +0900

    main_page 초안

--- Trust_it/urls.py
+++ Trust_it/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,6 +23,7 @@ urlpatterns = [
     path('communication/',include('communication.urls')),
     path('analysisproducts/',include('analysisproducts.urls')),
     path('notification/',include('notification.urls')),
+    path('', include('main.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
+def main(request):
+
+    return render(
+        request,
+        'main/mainpage.html',
+    )
+
 # Create your views here.
