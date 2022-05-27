c7c0f8674d125159cf350e1c2a3b9820bcb7503b
dahyun0917 <dahyun0917@naver.com>
2022-05-28 00:17:52 +0900

    제품분석 페이지 화면 수정

--- analysisproducts/urls.py
+++ analysisproducts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductList.as_view()),
-    path('company/<str:slug>/',views.company_page),
+    #path('company/<str:slug>/',views.company_page),
+    path('category/<str:slug>/',views.category_page),
     path('<int:pk>/', views.ProductDetail.as_view()),
 ]
--- analysisproducts/views.py
+++ analysisproducts/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product,Company, Category
+from .models import Product, Category
+   
+class ProductList(ListView):
+    model= Product
+    #ordering = '-pk'
+    paginate_by = 12
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProductList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_product_count'] = Product.objects.filter(
+            category=None).count()
+        return context
+
+class ProductDetail(DetailView):
+    model = Product
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_product_count'] = Product.objects.filter(
+            category=None).count()
+        return context 
 
 def category_page(request, slug):
     if slug == 'no_category':
-        category ='미분류'
+        category ='기타'
         product_list = Product.objects.filter(category=None)
     else:
         category = Category.objects.get(slug=slug)
@@ -20,35 +42,4 @@ def category_page(request, slug):
             'category': category,
         }
 
-    )
-def company_page(request, slug):
-    company = Company.objects.get(slug=slug)
-    product_list = company.post_set.all()
-
-    return render(
-        request,
-        'analysisproducts/product_list.html',
-        {
-            'product_list': product_list,
-            'company': company,
-        }
-    )
-        
-class ProductList(ListView):
-    model= Product
-    #ordering = '-pk'
-    paginate_by = 12
-    
-    def get_context_data(self, **kwargs):
-        context = super(ProductList, self).get_context_data()
-        return context
-
-class ProductDetail(DetailView):
-    model = Product
-    
-    def get_context_data(self, **kwargs):
-        context = super(ProductDetail, self).get_context_data()
-        
-        return context 
-
-# Create your views here.
+    )  
